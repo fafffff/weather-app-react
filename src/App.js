c50b529c3b803ef3a655c3f849c2e33a5967b357
Fasika Belayneh <fafibelayneh@gmail.com>
2024-10-05 11:56:12 +0300

    added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <p>
-          This project was coded by{" "}
-          <a href="https://github.com/fafffff" target="_blank" rel="noreferrer">
-            Fasika Belayneh
-          </a>{" "}
-          and is
-          <a
-            href="https://github.com/fafffff/weather-app-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open-sourced
-          </a>
-        </p>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://github.com/fafffff"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Fasika Belayneh
+            </a>{" "}
+            and is
+            <a
+              href="https://github.com/fafffff/weather-app-react"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-sourced
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
