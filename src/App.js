f859f733765bb452bd21e25b51f32f73469680b5
Fasika Belayneh <fafibelayneh@gmail.com>
2024-10-05 11:36:07 +0300

    added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <p>
+          This project was coded by{" "}
+          <a href="https://github.com/fafffff" target="_blank" rel="noreferrer">
+            Fasika Belayneh
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/fafffff/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
